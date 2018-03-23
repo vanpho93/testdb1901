91353f07b2f028a545860ef71e03352f8bb274a5
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-23 18:53:42 +0700

    databaseUri

--- src/helpers/connectDatabase.js
+++ src/helpers/connectDatabase.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
 
+function getDatabaseUri() {
+    if (process.env.NODE_ENV === 'test') return 'mongodb://localhost/project1901-test';
+    // if (process.env.NODE_ENV === 'production') return 'mongodb://localhost/project1901-test';
+    return 'mongodb://localhost/project1901'
+}
+
 mongoose.connect('mongodb://localhost/project1901')
 .catch(() => process.exit(1));
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,8 @@
 require('../src/helpers/connectDatabase');
 const { Story } = require('../src/models/story.model');
 
+process.env.NODE_ENV = 'test';
+
 beforeEach('Remove database for test', async () => {
     await Story.remove({});
 });
