ba43d74c2db034df07b3f3c19ccc8dcfbc4473e6
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-09 18:39:53 +0700

    update Story

--- test/controllers/story/updateStory.route.test.js
+++ test/controllers/story/updateStory.route.test.js
@@ -20,24 +20,76 @@ describe('PUT /story/:_id', () => {
         idStory = story._id
     });
 
+
     it('Can update story', async () => {
+        const response = await request(app)
+        .put('/story/' + idStory)
+        .send({ content: 'xyz' })
+        .set({ token: token1 });
+        assert.equal(response.body.success, true);
+        assert.equal(response.body.story._id, idStory);
+        assert.equal(response.body.story.content, 'xyz');
+        const story = await Story.findOne({});
+        assert.equal(story.content, 'xyz');
     });
 
     it('Cannot update a story without content', async () => {
+        const response = await request(app)
+        .put('/story/' + idStory)
+        .send({ content: '' })
+        .set({ token: token1 });
+        assert.equal(response.body.success, false);
+        assert.equal(response.body.code, 'CONTENT_NOT_EMPTY');
+        assert.equal(response.status, 400);
     });
 
     it('Cannot update story without token', async () => {
+        const response = await request(app)
+        .put('/story/' + idStory)
+        .send({ content: 'a' })
+        assert.equal(response.body.success, false);
+        assert.equal(response.body.code, 'INVALID_TOKEN');
+        assert.equal(response.status, 400);
     });
 
     it('Cannot update story with invalid token format', async () => {
+        const response = await request(app)
+        .put('/story/' + idStory)
+        .send({ content: 'a' })
+        .set({ token: 'x.y' })
+        assert.equal(response.body.success, false);
+        assert.equal(response.body.code, 'INVALID_TOKEN');
+        assert.equal(response.status, 400);
     });
 
     it('Cannot update story with other\'s token', async () => {
+        const response = await request(app)
+        .put('/story/' + idStory)
+        .send({ content: 'a' })
+        .set({ token: token2 })
+        assert.equal(response.body.success, false);
+        assert.equal(response.body.code, 'CANNOT_FIND_STORY');
+        assert.equal(response.status, 404);
     });
 
     it('Cannot update story with wrong story id', async () => {
+        const response = await request(app)
+        .put('/story/' + 'xyz')
+        .send({ content: 'a' })
+        .set({ token: token2 })
+        assert.equal(response.body.success, false);
+        assert.equal(response.body.code, 'INVALID_ID');
+        assert.equal(response.status, 400);
     });
 
     it('Cannot update a removed story', async () => {
+        await Story.findByIdAndRemove(idStory);
+        const response = await request(app)
+        .put('/story/' + idStory)
+        .send({ content: 'a' })
+        .set({ token: token1 })
+        assert.equal(response.body.success, false);
+        assert.equal(response.body.code, 'CANNOT_FIND_STORY');
+        assert.equal(response.status, 404);
     });
 });
