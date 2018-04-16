312934d7aeb61ce8e44c193bd951e464c9317b86
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-16 18:59:47 +0700

    remove

--- test/controllers/friend/declineFriendRequest.route.test.js
+++ test/controllers/friend/declineFriendRequest.route.test.js
@@ -4,7 +4,7 @@ const { app } = require('../../../src/app');
 const { Friend } = require('../../../src/models/friend.model');
 const { User } = require('../../../src/models/user.model.js');
 
-describe('POST /friend/accept/:_id', () => {
+describe('POST /friend/decline/:_id', () => {
     let token1, idUser1, token2, idUser2, idUser3, token3;
 
     beforeEach('Create users for test', async () => {
@@ -23,83 +23,67 @@ describe('POST /friend/accept/:_id', () => {
         await Friend.sendFriendRequest(idUser1, idUser2);
     });
 
-    it('Can accept friend request', async () => {
+    it('Can decline friend request', async () => {
         const response = await request(app)
-        .post('/friend/accept/' + idUser1)
+        .post('/friend/decline/' + idUser1)
         .set({ token: token2 })
         .send({});
         assert.equal(response.body.success, true);
-        assert.equal(response.body.friend._id, idUser1);
-        assert.equal(response.body.friend.name, 'Teo');
-        const sender = await User.findById(idUser1).populate('friends');
-        const receiver = await User.findById(idUser2).populate('friends');
-        assert.equal(sender.friends[0]._id.toString(), idUser2);
-        assert.equal(receiver.friends[0]._id.toString(), idUser1);
+        assert.equal(response.body.requestor._id, idUser1);
+        assert.equal(response.body.requestor.name, 'Teo');
+        const sender = await User.findById(idUser1).populate('sentRequests');
+        const receiver = await User.findById(idUser2).populate('incommingRequests');
+        assert.equal(sender.sentRequests.length, 0);
+        assert.equal(receiver.incommingRequests.length, 0);
     });
 
-    it('Cannot accept friend request for user, that have not sent you request', async () => {
+    it('Cannot decline friend request for user, that have not sent you request', async () => {
         const response = await request(app)
-        .post('/friend/accept/' + idUser3)
+        .post('/friend/decline/' + idUser3)
         .set({ token: token2 })
         .send({});
         assert.equal(response.body.success, false);
         assert.equal(response.body.code, 'CANNOT_FIND_USER');
         assert.equal(response.status, 404);
-        const user1 = await User.findById(idUser1);
-        const user2 = await User.findById(idUser2);
-        const user3 = await User.findById(idUser3);
-        assert.equal(user1.friends.length, 0);
-        assert.equal(user1.friends.length, 0);
-        assert.equal(user1.friends.length, 0);
     });
 
-    it('Cannot accept friend request for user twice', async () => {
-        await Friend.acceptRequest(idUser2, idUser1);
+    it('Cannot decline friend request for user twice', async () => {
+        await Friend.declineRequest(idUser2, idUser1);
         const response = await request(app)
-        .post('/friend/accept/' + idUser1)
+        .post('/friend/decline/' + idUser1)
         .set({ token: token2 })
         .send({});
         assert.equal(response.body.success, false);
         assert.equal(response.body.code, 'CANNOT_FIND_USER');
         assert.equal(response.status, 404);
-        const user1 = await User.findById(idUser1);
-        const user2 = await User.findById(idUser2);
-        const user3 = await User.findById(idUser3);
-        assert.equal(user1.friends.length, 1);
-        assert.equal(user2.friends.length, 1);
-        assert.equal(user3.friends.length, 0);
     });
 
-    it('Cannot accept friend request for removed user', async () => {
+    it('Cannot decline friend request for removed user', async () => {
         await User.findByIdAndRemove(idUser1);
         const response = await request(app)
-        .post('/friend/accept/' + idUser1)
+        .post('/friend/decline/' + idUser1)
         .set({ token: token2 })
         .send({});
         assert.equal(response.body.success, false);
         assert.equal(response.body.code, 'CANNOT_FIND_USER');
         assert.equal(response.status, 404);
-        const user2 = await User.findById(idUser2).populate('friends');
-        assert.equal(user2.friends.length, 0);
     });
 
-    it('Cannot accept friend request with removed user', async () => {
+    it('Cannot decline friend request with removed user', async () => {
         await User.findByIdAndRemove(idUser2);
         const response = await request(app)
-        .post('/friend/accept/' + idUser1)
+        .post('/friend/decline/' + idUser1)
         .set({ token: token2 })
         .send({});
         assert.equal(response.body.success, false);
         assert.equal(response.body.code, 'CANNOT_FIND_USER');
         assert.equal(response.status, 404);
-        const user1 = await User.findById(idUser1).populate('friends');
-        assert.equal(user1.friends.length, 0);
     });
 
-    it('Cannot accept friend request without token', async () => {
+    it('Cannot decline friend request without token', async () => {
         await User.findByIdAndRemove(idUser2);
         const response = await request(app)
-        .post('/friend/accept/' + idUser1)
+        .post('/friend/decline/' + idUser1)
         .send({});
         assert.equal(response.body.success, false);
         assert.equal(response.body.code, 'INVALID_TOKEN');
