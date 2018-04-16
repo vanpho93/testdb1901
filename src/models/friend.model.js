f983275ee397b40a37b021ebfacf5248a4a948e6
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-16 17:36:21 +0700

    init 1

--- src/models/friend.model.js
+++ src/models/friend.model.js
@@ -1,7 +1,22 @@
 const { User } = require('./user.model');
+const { validateObjectIds, validateUserExist } = require('../helpers/validators');
 
 class Friend {
-    static async sendFriendRequest(idUser, idReceiver) {}
+    static async sendFriendRequest(idUser, idReceiver) {
+        validateObjectIds(idUser, idReceiver);
+        const queryObj = {
+            _id: idUser,
+            friends: { $ne: idReceiver },
+            incommingRequests: { $ne: idReceiver },
+            sentRequests: { $ne: idReceiver },
+        };
+        const updateObj = { $push: { sentRequests: idReceiver } };
+        const receiver = await User.findByIdAndUpdate(idReceiver, { $push: { incommingRequests: idUser } });
+        validateUserExist(receiver);
+        const user = await User.findOneAndUpdate(queryObj, updateObj, { new: true });
+        validateUserExist(user);
+        return user;
+    }
     static async removeFriendRequest(idUser, idReceiver) {}
     static async acceptRequest(idUser, idRequestUser) {}
     static async declineRequest(idUser, idRequestUser) {}
